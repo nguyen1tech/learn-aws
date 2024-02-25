6f00d07b1d290519f018b070b502d58146b5ed6f
nguyen1tech <nvnguyen0089@gmail.com>
2024-02-25 15:50:16 +0700

    My first sam example code with python

---
+++ sam/sam-example-python/commands.sh
@@ -0,0 +1,8 @@
+# create s3 bucket
+aws s3 mb s3://bac-sam-code
+
+# package cloudformation
+aws cloudformation package --s3-bucket bac-sam-code --template-file template.yaml --output-template-file gen/template-generated.yaml
+
+# deploy sam
+aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name hello-word-sam --capabilities CAPABILITY_IAM
